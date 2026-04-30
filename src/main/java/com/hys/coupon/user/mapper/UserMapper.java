fc95e9de02568dbde3d20249e59538cabd4c6296
harryJJ5 <harryJJ.h.liao@outlook.com>
2026-04-30 17:31:23 +0800

    chore: initialize project from platform template

---
+++ src/main/java/com/hys/coupon/user/mapper/UserMapper.java
@@ -0,0 +1,9 @@
+package com.hys.coupon.user.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.hys.coupon.user.entity.UserEntity;
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface UserMapper extends BaseMapper<UserEntity> {
+}
